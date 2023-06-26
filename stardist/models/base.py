492bbc3624fd00135a97476574b3985ef89f3427
Utkarsh Kanwat <utkarshkanwat@gmail.com>
2023-06-27 04:20:11 +0530

    Update base.py

--- stardist/models/base.py
+++ stardist/models/base.py
@@ -303,8 +303,11 @@ def compound_tversky_cce(weights, ndim, alpha=0.7, gamma=0):
         ## --------------------------------
 
         ## PanNuke -----------------------------
-        rep_list = [4,5,6,7,8,12]
+        rep_list = [4,5,6,7,8]
         add_list = [3,4,5,6,7]
+        y_pred = add_pred_vals(y_pred, add_list, rep_list)
+        rep_list = [10,11,12]
+        add_list = [9,10,11,12]
         y_pred = add_pred_vals(y_pred, add_list, rep_list)
         ## ---------------------------------------
 
@@ -900,8 +903,11 @@ class StarDistBase(BaseModel):
         ## --------------------------------
 
         ## PanNuke -----------------------------
-        rep_list = [4,5,6,7,8,12]
+        rep_list = [4,5,6,7,8]
         add_list = [3,4,5,6,7]
+        result[2] = out_add_pred_vals(result[2], add_list, rep_list)
+        rep_list = [10,11,12]
+        add_list = [9,10,11,12]
         result[2] = out_add_pred_vals(result[2], add_list, rep_list)
         ## ---------------------------------------
 
